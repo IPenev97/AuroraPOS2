fb24733fe7bd71b6eb7098b4aa2ab13550306c4d
Ivo <I.Penev97@gmail.com>
2023-07-11 15:52:11 +0300

    Commit 04/07/23

---
+++ app/src/main/java/com/sistechnology/aurorapos2/feature_clients/ui/ClientScreenViewModel.kt
@@ -0,0 +1,11 @@
+package com.sistechnology.aurorapos2.feature_clients.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ClientScreenViewModel @Inject constructor(
+
+) : ViewModel() {
+}
---
+++ app/src/main/java/com/sistechnology/aurorapos2/feature_clients/ui/ClientsScreen.kt
@@ -0,0 +1,10 @@
+package com.sistechnology.aurorapos2.feature_clients.ui
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun ClientsScreen(
+    viewModel: ClientScreenViewModel = hiltViewModel()
+) {
+}
